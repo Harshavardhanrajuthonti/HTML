eec2f622708a36edbb3ade815573652b98141d3a
Harshavardhanrajuthonti <144204579+Harshavardhanrajuthonti@users.noreply.github.com>
2025-09-09 19:21:29 +0530

    Add files via upload

---
+++ overflow.c
@@ -0,0 +1,15 @@
+<html>
+<head>
+</head>
+<style>
+.class{
+background-color:skyblue;
+color:red;
+overflow:scroll;
+</style>
+<body>
+<p class="har">
+I qwertyuipoasdfghjklzxcvbnm mnbvcxasdfg iuytrewqas nbvc iuy tre jhgfd bty
+sdfg wertyu kjh zxc mnb i</p>
+</body>
+</html>
---
+++ tutorial.c
@@ -0,0 +1,7 @@
+<html>
+</head></head>
+<title>web design</title>
+<body>
+<b>harsha<sup>vardhan</sup>Raju<sub>thonti</sub></b>
+</body>
+</html>
